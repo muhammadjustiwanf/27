2fe5669d91e81f96524c6e6b6442838c601dae19
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-08-21 17:50:13 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-import requests, random
+import time, random, sys, json, codecs, threading, glob, re, string, os, requests, six, ast, pytz, urllib3, urllib.parse, traceback, atexit, wikipedia, goslate, html5lib
+
+from gtts import gTTS
+from time import sleep
+from datetime import datetime, timedelta
+from googletrans import Translator
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+from humanfriendly import format_timespan, format_size, format_number, format_length
+
 from bs4 import BeautifulSoup
 
 from flask import Flask, request, abort
@@ -15,6 +25,173 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi('HjEZRNk3czUVKof7ZLxIO3Bv8zdkeW1UBTPl9HNMmYgVHtQapRJr2ZyVB8qOMVrdmkTDfZ7nRjnavXF8xgO9qeBVd47MSgfP3k0J+oPWjw8+8dli+crm5VKHcFi+xidY2razYmls+0EC9CxIYnbOYwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('a4ce4df86f3b49ce0ab0a91f1f4fec16')
+
+list_language = {
+    "list_textToSpeech": {
+        "id": "Indonesia",
+        "af" : "Afrikaans",
+        "sq" : "Albanian",
+        "ar" : "Arabic",
+        "hy" : "Armenian",
+        "bn" : "Bengali",
+        "ca" : "Catalan",
+        "zh" : "Chinese",
+        "zh-cn" : "Chinese (Mandarin/China)",
+        "zh-tw" : "Chinese (Mandarin/Taiwan)",
+        "zh-yue" : "Chinese (Cantonese)",
+        "hr" : "Croatian",
+        "cs" : "Czech",
+        "da" : "Danish",
+        "nl" : "Dutch",
+        "en" : "English",
+        "en-au" : "English (Australia)",
+        "en-uk" : "English (United Kingdom)",
+        "en-us" : "English (United States)",
+        "eo" : "Esperanto",
+        "fi" : "Finnish",
+        "fr" : "French",
+        "de" : "German",
+        "el" : "Greek",
+        "hi" : "Hindi",
+        "hu" : "Hungarian",
+        "is" : "Icelandic",
+        "id" : "Indonesian",
+        "it" : "Italian",
+        "ja" : "Japanese",
+        "km" : "Khmer (Cambodian)",
+        "ko" : "Korean",
+        "la" : "Latin",
+        "lv" : "Latvian",
+        "mk" : "Macedonian",
+        "no" : "Norwegian",
+        "pl" : "Polish",
+        "pt" : "Portuguese",
+        "ro" : "Romanian",
+        "ru" : "Russian",
+        "sr" : "Serbian",
+        "si" : "Sinhala",
+        "sk" : "Slovak",
+        "es" : "Spanish",
+        "es-es" : "Spanish (Spain)",
+        "es-us" : "Spanish (United States)",
+        "sw" : "Swahili",
+        "sv" : "Swedish",
+        "ta" : "Tamil",
+        "th" : "Thai",
+        "tr" : "Turkish",
+        "uk" : "Ukrainian",
+        "vi" : "Vietnamese",
+        "cy" : "Welsh"
+    },
+    "list_translate": {    
+        "af": "afrikaans",
+        "sq": "albanian",
+        "am": "amharic",
+        "ar": "arabic",
+        "hy": "armenian",
+        "az": "azerbaijani",
+        "eu": "basque",
+        "be": "belarusian",
+        "bn": "bengali",
+        "bs": "bosnian",
+        "bg": "bulgarian",
+        "ca": "catalan",
+        "ceb": "cebuano",
+        "ny": "chichewa",
+        "zh-cn": "chinese (simplified)",
+        "zh-tw": "chinese (traditional)",
+        "co": "corsican",
+        "hr": "croatian",
+        "cs": "czech",
+        "da": "danish",
+        "nl": "dutch",
+        "en": "english",
+        "eo": "esperanto",
+        "et": "estonian",
+        "tl": "filipino",
+        "fi": "finnish",
+        "fr": "french",
+        "fy": "frisian",
+        "gl": "galician",
+        "ka": "georgian",
+        "de": "german",
+        "el": "greek",
+        "gu": "gujarati",
+        "ht": "haitian creole",
+        "ha": "hausa",
+        "haw": "hawaiian",
+        "iw": "hebrew",
+        "hi": "hindi",
+        "hmn": "hmong",
+        "hu": "hungarian",
+        "is": "icelandic",
+        "ig": "igbo",
+        "id": "indonesian",
+        "ga": "irish",
+        "it": "italian",
+        "ja": "japanese",
+        "jw": "javanese",
+        "kn": "kannada",
+        "kk": "kazakh",
+        "km": "khmer",
+        "ko": "korean",
+        "ku": "kurdish (kurmanji)",
+        "ky": "kyrgyz",
+        "lo": "lao",
+        "la": "latin",
+        "lv": "latvian",
+        "lt": "lithuanian",
+        "lb": "luxembourgish",
+        "mk": "macedonian",
+        "mg": "malagasy",
+        "ms": "malay",
+        "ml": "malayalam",
+        "mt": "maltese",
+        "mi": "maori",
+        "mr": "marathi",
+        "mn": "mongolian",
+        "my": "myanmar (burmese)",
+        "ne": "nepali",
+        "no": "norwegian",
+        "ps": "pashto",
+        "fa": "persian",
+        "pl": "polish",
+        "pt": "portuguese",
+        "pa": "punjabi",
+        "ro": "romanian",
+        "ru": "russian",
+        "sm": "samoan",
+        "gd": "scots gaelic",
+        "sr": "serbian",
+        "st": "sesotho",
+        "sn": "shona",
+        "sd": "sindhi",
+        "si": "sinhala",
+        "sk": "slovak",
+        "sl": "slovenian",
+        "so": "somali",
+        "es": "spanish",
+        "su": "sundanese",
+        "sw": "swahili",
+        "sv": "swedish",
+        "tg": "tajik",
+        "ta": "tamil",
+        "te": "telugu",
+        "th": "thai",
+        "tr": "turkish",
+        "uk": "ukrainian",
+        "ur": "urdu",
+        "uz": "uzbek",
+        "vi": "vietnamese",
+        "cy": "welsh",
+        "xh": "xhosa",
+        "yi": "yiddish",
+        "yo": "yoruba",
+        "zu": "zulu",
+        "fil": "Filipino",
+        "he": "Hebrew"
+    }
+}
 
 
 @app.route("/callback", methods=['POST'])
@@ -33,6 +210,15 @@ def callback():
         abort(400)
 
     return 'OK'
+
+    text = (event.message.text).lower()
+    msg = text.split()
+    groupId = event.source.group_id
+    userId = event.source.user_id
+    profile = line_bot_api.get_profile(userId)
+    profile_name = profile.display_name
+    profile_picture = profile.picture_url
+    profile_sm = profile.status_message
 
 
 def apple_news():
